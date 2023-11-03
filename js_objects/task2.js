8150b8c17604c3143cd03c750fc97c4f858f5e90
lozin <lozinskaya.nadia@gmail.com>
2023-11-03 19:25:24 +0200

    besedina // feat // add files with tasks 1-4 from topic objects

---
+++ js_objects/task2.js
@@ -0,0 +1,8 @@
+const book = {
+    title: `City of girl`,
+    author: `Elizabeth Gilbert`,
+    year: 2019
+}
+
+console.log(`The book name:`, book.title)
+console.log(`The book's author:`, book.author)
